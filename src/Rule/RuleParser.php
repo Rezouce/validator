3b3705583c2fa5f6a20f8a7bc9d6170117580c4f
Rezouce <contact@rezouce.net>
2018-02-03 22:03:11 +0100

    Improves the parsing of options from a string representing a rule. It cast numeric values and doesn't send unique option as array anymore.

--- src/Rule/RuleParser.php
+++ src/Rule/RuleParser.php
@@ -18,12 +18,36 @@ class RuleParser
 
         $options = array_map(function ($option) {
             if (is_string($option) && !empty($option)) {
-                return explode(',', $option);
+                return $this->getOptionsFromString($option);
             }
 
             return $option;
         }, $rule ?: []);
 
         return new Rule($name, $options);
+    }
+
+    /**
+     * When there is only one option provided, we return this options instead
+     * of an array containing it.
+     */
+    private function getOptionsFromString($option)
+    {
+        $options = array_map(function ($data) {
+            return $this->castData($data);
+        }, explode(',', $option));
+
+        return count($options) > 1
+            ? $options
+            : current($options);
+    }
+
+    /**
+     * When the data are provided by a string, we lost the original casting.
+     * This method cast the numeric values from string to float/int.
+     */
+    private function castData($data)
+    {
+        return is_numeric($data) ? 0 + $data : $data;
     }
 }
--- tests/Rule/RuleParserTest.php
+++ tests/Rule/RuleParserTest.php
@@ -33,6 +33,16 @@ class RuleParserTest extends TestCase
     }
 
     /** @test */
+    public function itParsesTheOptionsAsStringWhenThereAreOnlyOneChoice()
+    {
+        $parser = new RuleParser();
+
+        $rule = $parser->parse('between:a:g');
+
+        $this->assertEquals(['a', 'g'], $rule->getOptions());
+    }
+
+    /** @test */
     public function itParsesArrayRuleToARuleObject()
     {
         $parser = new RuleParser();
@@ -66,5 +76,15 @@ class RuleParserTest extends TestCase
         $this->assertInstanceOf(Rule::class, $rule);
         $this->assertEquals('in', $rule->getName());
         $this->assertEquals([['jedi', 'sith']], $rule->getOptions());
+    }
+
+    /** @test */
+    public function itRecastNumericValuesInStringRules()
+    {
+        $parser = new RuleParser();
+
+        $rule = $parser->parse('min:12.5');
+
+        $this->assertEquals([12.5], $rule->getOptions());
     }
 }
